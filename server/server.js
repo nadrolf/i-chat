bb52889ab538049c006eb23ff37208b85d7b3606
Flordan delos Reyes <nadrolf13@gmail.com>
2018-11-18 13:54:12 +0800

    work in progress

--- server/server.js
+++ server/server.js
@@ -3,42 +3,70 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app)
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user Connected');
 
-    //emit from Admin text Welcome to the chat app
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            return callback('Name and room are required.');
+        }
 
-    //emit from Admin to new users
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        //emit from Admin text Welcome to the chat app
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+        //emit from Admin to new users
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+
+        callback();
+    });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
+        var user = users.getUser(socket.id);
 
-        // send to all including sender
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        if (user && isRealString(message.text)) {
+            // send to all including sender
+            //io.emit('newMessage', generateMessage(message.from, message.text));
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
+      
         callback('This is from the server');
-        // send to all except sender
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
     })
+
+    socket.on('createLocationMessage', (coords)=> {
+        var user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
+    });
 
     socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    })
+        var user = users.removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+        }
+    });
 });
 
 server.listen(port, () => {
